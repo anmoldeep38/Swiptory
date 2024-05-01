6ee7d4c137ba2fa876c1c3e632fd0a49db36f4f2
Anmol Deep <87927563+anmoldeep38@users.noreply.github.com>
2024-05-01 14:31:27 +0530

    Delete client directory

--- client/src/pages/Bookmarks.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import UserBookmarks from "../components/user/Bookmarks/Bookmarks";
-
-const Bookmarks = () =>{
-  return <UserBookmarks/>;
-};
-
-export default Bookmarks;
--- client/src/pages/UserStories.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import Stories from '../components/user/Stories/Stories';
-const UserStories = () => {
-  return (
-    <Stories/>
-  )
-}
-
-export default UserStories
